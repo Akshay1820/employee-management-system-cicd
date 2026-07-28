9e279c64fe943161707c08801e61591bb95dffbb
Akshay1820 <akshuvaidya1@gmail.com>
2026-07-28 09:58:20 +0530

    Added test cases which will fail

---
+++ src/test/java/com/employee_management/cicd/controller/EmployeeControllerTest.java
@@ -0,0 +1,20 @@
+package com.employee_management.cicd.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class EmployeeControllerTest {
+
+    private final EmployeeController employeeController = new EmployeeController();
+
+    @Test
+    void getAllEmployeesReturnsSixEmployees() {
+        var response = employeeController.getAllEmployees();
+
+        assertEquals(200, response.getStatusCode().value());
+        assertNotNull(response.getBody());
+        assertEquals(5, response.getBody().size());
+    }
+}
